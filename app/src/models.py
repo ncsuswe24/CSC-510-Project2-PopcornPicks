cb8f7b390fbd6152054a4b84680ddcb3da24217c
jlakhan <jlakhan@ncsu.edu>
2024-11-25 07:51:23 -0500

    Added likes and dislikes feature

--- app/src/models.py
+++ app/src/models.py
@@ -26,6 +26,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     reviews = db.relationship('Review', backref='user_author', lazy=True)
+    likes = db.relationship('Like', backref='user', lazy=True)
 
     def __repr__(self):
         return f" {self.first_name} {self.last_name}"
@@ -43,7 +44,9 @@ class Movie(db.Model):
     imdb_id = db.Column(db.String(20), nullable=False)
     poster_path = db.Column(db.String(200), nullable=True)
     reviews = db.relationship('Review', backref='movie_author', lazy=True)
-
+    total_likes = db.Column(db.Integer, default=0)
+    total_dislikes = db.Column(db.Integer, default=0)
+    likes = db.relationship('Like', backref='movie', lazy=True)
     def __repr__(self):
         return f"{self.movieId} - {self.title}"
 
@@ -71,3 +74,15 @@ class ListMovie(db.Model):
 
     def __repr__(self):
         return f"{self.user_id} - {self.movieId}"
+    
+class Like(db.Model):
+    """
+    Database model for a Like.
+    """
+    like_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    movieId = db.Column(db.Integer, db.ForeignKey('movie.movieId'), nullable=False)
+    like_value = db.Column(db.Integer, nullable=False)  
+
+    def __repr__(self):
+        return f"{self.user_id} {'liked' if self.like_value == 1 else 'disliked'} {self.movieId}"
