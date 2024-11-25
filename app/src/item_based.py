1dec85333c7cb8c1a30a53a92adf94d212387a61
fameen <fameen@ncsu.edu>
2024-11-25 16:46:42 -0500

    Incorporating more pylint suggestions

--- app/src/item_based.py
+++ app/src/item_based.py
@@ -37,18 +37,19 @@ def recommend_for_new_user(user_rating, selected_genre=None, selected_year=None)
                 "poster_path", "runtime", "recommended"
             ]
         )
-    
+
     if not user_rating:
         return default
-    
+
     # Load movies dataset and embeddings
     movies = pd.read_csv(os.path.join(project_dir, "data", "movies.csv"))
-    embeddings = np.load(os.path.join(project_dir, "data", "movie_embeddings.npz"))['embeddings']  # Shape: (num_movies, embedding_size)
+    embeddings = np.load(
+        os.path.join(project_dir, "data", "movie_embeddings.npz"))['embeddings']
     # np.savez_compressed("embeddings.npz", embeddings=embeddings)
     user_ratings_df = pd.DataFrame(user_rating)  # Expects keys: 'movieId', 'rating'
 
     # Find the row indices for the user-rated movies in the movies dataset
-    user_indices = movies[movies['title'].isin(user_ratings_df['title'])].index.to_numpy()  # Row indices in the movies dataset
+    user_indices = movies[movies['title'].isin(user_ratings_df['title'])].index.to_numpy()
 
     # Extract the corresponding embeddings and ratings
     user_embeddings = embeddings[user_indices]
@@ -82,61 +83,3 @@ def recommend_for_new_user(user_rating, selected_genre=None, selected_year=None)
 
     # Return top 10 recommendations
     return recommendations.head(9)
-
-
-# def recommend_for_new_user(): # pylint: disable=too-many-locals
-#     """
-#     Generates a list of recommended movie titles for a new user based on their ratings.
-#     """
-#     if not user_rating:
-#         return pd.DataFrame(
-#             columns=[
-#                 "movieId", "title", "genres", "imdb_id", "overview",
-#                 "poster_path", "runtime", "recommended"
-#             ]
-#         )
-
-#     # ratings = pd.read_csv(os.path.join(project_dir, "data", "ratings.csv"))
-#     movies = pd.read_csv(os.path.join(project_dir, "data", "movies.csv"))
-#     user = pd.DataFrame(user_rating)
-#     user_movie_id = movies[movies["title"].isin(user["title"])]
-#     user_ratings = pd.merge(user_movie_id, user)
-
-#     movies_genre_filled = movies.copy(deep=True)
-#     copy_of_movies = movies.copy(deep=True)
-
-#     for index, row in copy_of_movies.iterrows():
-#         copy_of_movies.at[index, "genres"] = row["genres"].split("|")
-
-#     for index, row in copy_of_movies.iterrows():
-#         for genre in row["genres"]:
-#             movies_genre_filled.at[index, genre] = 1
-
-#     movies_genre_filled = movies_genre_filled.fillna(0)
-
-#     user_genre = movies_genre_filled[movies_genre_filled.movieId.isin(user_ratings.movieId)]
-#     user_genre.drop(["movieId", "title", "genres", "imdb_id", "overview", "poster_path", "runtime"],
-#                      axis=1, inplace=True)
-#     user_profile = user_genre.T.dot(user_ratings.rating.to_numpy())
-
-#     movies_genre_filled.set_index(movies_genre_filled.movieId)
-#     movies_genre_filled.drop(["movieId", "title", "genres", "imdb_id", "overview", "poster_path",
-#                                "runtime"], axis=1, inplace=True)
-
-#     recommendations = (movies_genre_filled.dot(user_profile)) / user_profile.sum()
-
-#     join_movies_and_recommendations = movies.copy(deep=True)
-#     join_movies_and_recommendations["recommended"] = recommendations
-#     join_movies_and_recommendations.sort_values(
-#         by="recommended", ascending=False, inplace=True
-#     )
-#     if selected_genre:
-#         join_movies_and_recommendations = join_movies_and_recommendations[
-#             join_movies_and_recommendations["genres"].str.contains(selected_genre, case=False)
-#         ]
-
-#     if selected_year:
-#         join_movies_and_recommendations = join_movies_and_recommendations[
-#             join_movies_and_recommendations["title"].apply(extract_year_from_title) == selected_year
-#         ]
-#     return join_movies_and_recommendations[:9]
